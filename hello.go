8aff84280ade297d3e252413dea124e29967f81a
Anand Nekkunti <nekkunti@gmail.com>
2016-01-20 00:45:40 +0530

    commit with pkg

--- hello.go
+++ hello.go
@@ -1,8 +1,9 @@
 package main
 
 import "fmt"
+import "github.com/anekkunt/stringutil_pkg"
 
 func main() {
-    fmt.Printf("hello, Anand11\n")
+	fmt.Printf("hello, Anand11\n")
+	stringutil_pkg.Reverse("Anand")
 }
-
